75efe38af3b2c2874eb0e9e602ab255b05968747
Daniel Mós <danielmos@yahoo.com>
2018-08-07 16:51:50 -0300

    Relação entre objetos

--- src/main/java/br/com/reliabletech/igrc/models/OrgUnit.java
+++ src/main/java/br/com/reliabletech/igrc/models/OrgUnit.java
@@ -1,9 +1,16 @@
 package br.com.reliabletech.igrc.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.Lob;
+import javax.persistence.ManyToMany;
 import javax.validation.constraints.NotNull;
 
 @Entity
@@ -29,6 +36,34 @@ public class OrgUnit {
 	private Boolean signoff;
 	
 	private String documents;
+	
+	@ManyToMany(cascade={CascadeType.PERSIST, CascadeType.MERGE})
+	@JoinTable(name="jt_orgunit_risk", joinColumns=@JoinColumn(name="orgunit_guid"), inverseJoinColumns=@JoinColumn(name="risk_guid"))
+	private List<Risk> risks = new ArrayList<Risk>();
+		
+	@ManyToMany(cascade={CascadeType.PERSIST, CascadeType.MERGE})
+	@JoinTable(name="jt_orgunit_control", joinColumns=@JoinColumn(name="orgunit_guid"), inverseJoinColumns=@JoinColumn(name="control_guid"))
+	private List<Control> controls = new ArrayList<Control>();
+
+	@ManyToMany(cascade={CascadeType.PERSIST, CascadeType.MERGE})
+	@JoinTable(name="jt_orgunit_reg", joinColumns=@JoinColumn(name="orgunit_guid"), inverseJoinColumns=@JoinColumn(name="regulation_guid"))
+	private List<Regulation> regulations = new ArrayList<Regulation>();
+	
+	@ManyToMany(cascade={CascadeType.PERSIST, CascadeType.MERGE})
+	@JoinTable(name="jt_orgunit_process", joinColumns=@JoinColumn(name="orgunit_guid"), inverseJoinColumns=@JoinColumn(name="process_guid"))
+	private List<Process> processes = new ArrayList<Process>();
+	
+	@ManyToMany(cascade={CascadeType.PERSIST, CascadeType.MERGE})
+	@JoinTable(name="jt_orgunit_appsys", joinColumns=@JoinColumn(name="orgunit_guid"), inverseJoinColumns=@JoinColumn(name="appsystem_guid"))
+	private List<AppSystem> appsystems = new ArrayList<AppSystem>();
+	
+	@ManyToMany(cascade={CascadeType.PERSIST, CascadeType.MERGE})
+	@JoinTable(name="jt_orgunit_finacc", joinColumns=@JoinColumn(name="orgunit_guid"), inverseJoinColumns=@JoinColumn(name="financialaccount_guid"))
+	private List<FinancialAccount> financialaccounts = new ArrayList<FinancialAccount>();
+	
+	@ManyToMany(cascade={CascadeType.PERSIST, CascadeType.MERGE})
+	@JoinTable(name="jt_orgunit_product", joinColumns=@JoinColumn(name="orgunit_guid"), inverseJoinColumns=@JoinColumn(name="product_guid"))
+	private List<Product> products = new ArrayList<Product>();
 
 	public String getName() {
 		return name;
@@ -92,6 +127,62 @@ public class OrgUnit {
 
 	public void setDocuments(String documents) {
 		this.documents = documents;
+	}
+
+	public List<Risk> getRisks() {
+		return risks;
+	}
+
+	public void setRisks(List<Risk> risks) {
+		this.risks = risks;
+	}
+
+	public List<Control> getControls() {
+		return controls;
+	}
+
+	public void setControls(List<Control> controls) {
+		this.controls = controls;
+	}
+
+	public List<Regulation> getRegulations() {
+		return regulations;
+	}
+
+	public void setRegulations(List<Regulation> regulations) {
+		this.regulations = regulations;
+	}
+
+	public List<Process> getProcesses() {
+		return processes;
+	}
+
+	public void setProcesses(List<Process> processes) {
+		this.processes = processes;
+	}
+
+	public List<AppSystem> getAppsystems() {
+		return appsystems;
+	}
+
+	public void setAppsystems(List<AppSystem> appsystems) {
+		this.appsystems = appsystems;
+	}
+
+	public List<FinancialAccount> getFinancialaccounts() {
+		return financialaccounts;
+	}
+
+	public void setFinancialaccounts(List<FinancialAccount> financialaccounts) {
+		this.financialaccounts = financialaccounts;
+	}
+
+	public List<Product> getProducts() {
+		return products;
+	}
+
+	public void setProducts(List<Product> products) {
+		this.products = products;
 	}
 
 	
--- src/main/java/br/com/reliabletech/igrc/models/Product.java
+++ src/main/java/br/com/reliabletech/igrc/models/Product.java
@@ -1,9 +1,16 @@
 package br.com.reliabletech.igrc.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.Lob;
+import javax.persistence.ManyToMany;
 import javax.validation.constraints.NotNull;
 
 @Entity
@@ -27,6 +34,34 @@ public class Product {
 	
 	private String documents;
 
+	@ManyToMany(cascade={CascadeType.PERSIST, CascadeType.MERGE})
+	@JoinTable(name="jt_product_risk", joinColumns=@JoinColumn(name="product_guid"), inverseJoinColumns=@JoinColumn(name="risk_guid"))
+	private List<Risk> risks = new ArrayList<Risk>();
+
+	@ManyToMany(cascade={CascadeType.PERSIST, CascadeType.MERGE})
+	@JoinTable(name="jt_product_control", joinColumns=@JoinColumn(name="product_guid"), inverseJoinColumns=@JoinColumn(name="control_guid"))
+	private List<Control> controls = new ArrayList<Control>();
+
+	@ManyToMany(cascade={CascadeType.PERSIST, CascadeType.MERGE})
+	@JoinTable(name="jt_product_reg", joinColumns=@JoinColumn(name="product_guid"), inverseJoinColumns=@JoinColumn(name="regulation_guid"))
+	private List<Regulation> regulations = new ArrayList<Regulation>();
+	
+	@ManyToMany(cascade={CascadeType.PERSIST, CascadeType.MERGE})
+	@JoinTable(name="jt_product_process", joinColumns=@JoinColumn(name="product_guid"), inverseJoinColumns=@JoinColumn(name="process_guid"))
+	private List<Process> processes = new ArrayList<Process>();
+	
+	@ManyToMany(cascade={CascadeType.PERSIST, CascadeType.MERGE})
+	@JoinTable(name="jt_product_appsys", joinColumns=@JoinColumn(name="product_guid"), inverseJoinColumns=@JoinColumn(name="appsystem_guid"))
+	private List<AppSystem> appsystems = new ArrayList<AppSystem>();
+	
+	
+	@ManyToMany(mappedBy="products")
+	private List<OrgUnit> orgunits = new ArrayList<OrgUnit>();
+	
+	@ManyToMany(mappedBy="products")
+	private List<FinancialAccount> financeaccounts = new ArrayList<FinancialAccount>();
+	
+	
 	public String getName() {
 		return name;
 	}
@@ -82,4 +117,62 @@ public class Product {
 	public void setCostCenter(String costCenter) {
 		this.costCenter = costCenter;
 	}
+
+	public List<Risk> getRisks() {
+		return risks;
+	}
+
+	public void setRisks(List<Risk> risks) {
+		this.risks = risks;
+	}
+
+	public List<Control> getControls() {
+		return controls;
+	}
+
+	public void setControls(List<Control> controls) {
+		this.controls = controls;
+	}
+
+	public List<Regulation> getRegulations() {
+		return regulations;
+	}
+
+	public void setRegulations(List<Regulation> regulations) {
+		this.regulations = regulations;
+	}
+
+	public List<Process> getProcesses() {
+		return processes;
+	}
+
+	public void setProcesses(List<Process> processes) {
+		this.processes = processes;
+	}
+
+	public List<AppSystem> getAppsystems() {
+		return appsystems;
+	}
+
+	public void setAppsystems(List<AppSystem> appsystems) {
+		this.appsystems = appsystems;
+	}
+
+	public List<OrgUnit> getOrgunits() {
+		return orgunits;
+	}
+
+	public void setOrgunits(List<OrgUnit> orgunits) {
+		this.orgunits = orgunits;
+	}
+
+	public List<FinancialAccount> getFinanceaccounts() {
+		return financeaccounts;
+	}
+
+	public void setFinanceaccounts(List<FinancialAccount> financeaccounts) {
+		this.financeaccounts = financeaccounts;
+	}
+	
+	
 }
